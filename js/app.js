a17bf93c7c8e617fa18e0d3b228e54d9a9cc193c
kirk <kirk.betzina@gmail.com>
2021-04-09 17:30:01 -0400

    started the markdown

---
+++ js/app.js
@@ -0,0 +1,42 @@
+/////////////////////////////////////
+//get data from google sheets
+/////////////////////////////////////
+
+$.ajax('https://spreadsheets.google.com/feeds/list/1gm0-doCsKu-xwV47uUdRMPSOmVrM_SQv7-Vyk5xQZwM/1/public/full?alt=json')
+//.then for when the data arrives
+
+.then((data) => {
+    console.log(data)
+
+    // map over data to create a simpler data set
+    const project = data.feed.entry.map((item) => {
+        return {
+            
+            description: item.gsx$description.$t,
+            giturl: item.gsx$giturl.$t,
+            image: item.gsx$image.$t,
+            liveurl: item.gsx$liveurl.$t,
+            project: item.gsx$project.$t,
+
+        }
+        
+    })
+    console.log(project)
+    ////////////////////////////////////
+    // jQuery to render your projects
+    ///////////////////////////////////
+
+
+
+
+
+
+
+    ////////////////////////////////////
+    // end jQuery here
+    ////////////////////////////////////
+})
+//.catch in case of an error
+.catch((error) => {
+    console.log(error)
+})
---
+++ portfolio.md
@@ -0,0 +1,19 @@
+# This Is My Portfolio Markdown
+
+
+## Portfolio Description 
+I want this portfolio to display my skills in a succinct and fashionable way without, being so complicated that the individuals visiting it. I value simplistic design, and ease of use.
+ prime example would be the portfolio site of *Matt Farely* (https://mattfarley.ca/).
+
+The tools I will be using to complete my website are:
+
+- HTML
+- CSS
+- Flexbox
+- Grid
+- Javascript
+- jQuery
+- Ajax
+- Bootstrap
+
+
