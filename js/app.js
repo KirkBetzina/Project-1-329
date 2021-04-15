6bb00ad88091a47803fd70c979ca220e968f71b0
kirk <kirk.betzina@gmail.com>
2021-04-15 11:04:47 -0400

    really mixing things up, and dang trying to wrestle JS

--- js/app.js
+++ js/app.js
@@ -26,12 +26,14 @@ $.ajax('https://spreadsheets.google.com/feeds/list/1gm0-doCsKu-xwV47uUdRMPSOmVrM
     // jQuery to render your projects
     ///////////////////////////////////
     const $section = $('<section>')
-    $section.attr('id', 'cards1')
+    $section.attr('id', 'cards')
     $('section').appendTo($('body'))
-    const $div = $('<div>').attr('id', 'cards')
+    const $div = $('<div>').attr('id', 'card')
     $div.appendTo($section)
 
-    
+
+
+
 
 
 
