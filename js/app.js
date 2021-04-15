020dbcf5e2ace70d53d7b99b62454ae86f77b1ff
kirk <kirk.betzina@gmail.com>
2021-04-15 16:37:48 -0400

    added to the js buttons

--- js/app.js
+++ js/app.js
@@ -26,20 +26,33 @@ $.ajax('https://spreadsheets.google.com/feeds/list/1gm0-doCsKu-xwV47uUdRMPSOmVrM
     // jQuery to render your projects
     ///////////////////////////////////
     const $card1 = $('<card1>')
-    URL ='https://git.generalassemb.ly/freakiestkirk/W01D02-Hw.git'
     $card1.appendTo("#cards")
-    $(':button1').text('Startup Matchmaker')
+    $(':button1').text('Startup Matchmaker');{
+    $('#card1').on('click', () => {
+        window.location.href = 'https://git.generalassemb.ly/freakiestkirk/W01D02-Hw';  
+    })
+       
+    }
+    
     
     const $card2 = $('<card2>')
-    URL = 'https://git.generalassemb.ly/freakiestkirk/W1D05-Homework.git'
-    $card2.appendTo('#cards')
-    $(':button2').text('code breakdowns')
-   
-    const $card3 = $('<card3>')
-    URL = 'https://git.generalassemb.ly/freakiestkirk/jQuery_LOTR.git' 
-    $card3.appendTo('#cards')
-    $(':button3').text('LOTR')
+    $card2.appendTo('#cards')    
+    $(':button2').text('code breakdowns');{
+    window.location.href = 'https://git.generalassemb.ly/freakiestkirk/W1D05-Homework';
+    $('#card2').on('click', () =>{
+        window.location.href = 'https://git.generalassemb.ly/freakiestkirk/W1D05-Homework';   
+    })
+    
+    }
 
+    const $card3 = $('<card3>')
+    $card3.appendTo('#cards')
+    $(':button3').text('LOTR');{
+    $('#card3').on('click', () =>{
+        window.location.href= 'https://git.generalassemb.ly/freakiestkirk/jQuery_LOTR';  
+    })
+      
+   }
 
 
     
